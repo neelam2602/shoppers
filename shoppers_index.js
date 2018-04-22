3e7807b99e87cf9c4481ba0e10b97393adb11502
neelam2602 <neelamsoni854@gmail.com>
2018-04-23 02:10:11 +0530

    shoppers

--- shoppers_index.js
+++ shoppers_index.js
@@ -1,13 +1,51 @@
 $(document).ready(function(){
-	// alert(1)
-	// slideshow
-	$(".slide_img").hide();
-	$(".slide_img:first").show();
-	setInterval(function(){
-		$(".slide_img:first").fadeOut(1000).next().fadeIn(1000).end()
-		.appendTo(".slideshow")
-	},3000);
-	// slideshow
+	// lower slideshow
+
+	slide_wi = $(".slider").width();
+	// alert(slide_wi);
+	slide_hi = Math.round($(".slider").height());
+	// alert(slide_hi);
+
+	$(".slider").css("float","left");
+	$(".mainslide").width(slide_wi * $(".slider").length);
+	$(".mainslide, .gallery").height(slide_hi);
+	$(".gallery").width(slide_wi);
+	$(".gallery").css("border","3px solid");
+	$(".gallery").css("overflow","hidden");
+	
+	// lower slideshow
+
+	// blackbox
+	
+	$(".blackbox").hide();
+	$(".crick_img").mouseenter(function(){
+		$(this).children(".blackbox").fadeIn(300);
+	})
+	$(".crick_img").mouseleave(function(){
+		$(this).children(".blackbox").fadeOut(300);
+	})
+
+	//blackbox
+
+$(".tab_class_contents").hide();
+	// $(".tab_class_contents:first").show();
+
+	$(".tabs_ul li").mouseenter(function(){
+		position = $(this).index();
+		// alert(position);
+		$(".tab_class_contents").hide();
+		$(".tab_class_contents").eq(position).show();
+		// $(".tab_class_contents").eq(position).slideToggle(500);
+	})
+	$(".tabs_ul li").mouseleave(function(){
+		position = $(this).index();
+		// alert(position);
+		$(".tab_class_contents").hide();
+		$(".tab_class_contents").eq(position).hide();
+
+		// $(".tab_class_contents").eq(position).slideToggle(500);
+	})
+	
 
 	
 })
